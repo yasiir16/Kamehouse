ea8a7dfca26ca3b21d4030d2dce883d39b9dd233
yasiir16 <yasirpineda@gmail.com>
2024-10-16 09:52:12 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
+function uploadFiles() {
+    const fileInput = document.getElementById('file-input');
+    const gallery = document.getElementById('photo-gallery');
 
+    for (const file of fileInput.files) {
+        const reader = new FileReader();
+        reader.onload = function(event) {
+            const img = document.createElement('img');
+            img.src = event.target.result;
+            gallery.appendChild(img);
+        };
+        reader.readAsDataURL(file);
+    }
+}
