4ec2fbdccf373604975c510331b34e56d1589823
yasiir16 <yasirpineda@gmail.com>
2024-10-16 10:01:26 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,9 +5,16 @@ function uploadFiles() {
     for (const file of fileInput.files) {
         const reader = new FileReader();
         reader.onload = function(event) {
-            const img = document.createElement('img');
-            img.src = event.target.result;
-            gallery.appendChild(img);
+            if (file.type.startsWith('image/')) {
+                const img = document.createElement('img');
+                img.src = event.target.result;
+                gallery.appendChild(img);
+            } else if (file.type.startsWith('video/')) {
+                const video = document.createElement('video');
+                video.src = event.target.result;
+                video.controls = true;
+                gallery.appendChild(video);
+            }
         };
         reader.readAsDataURL(file);
     }
